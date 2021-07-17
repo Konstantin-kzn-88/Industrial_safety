0494120f2bb0ed4ae51c67d4cfb0699e68c97b68
konstantin <kuznetsovkm@yandex.ru>
2021-07-17 18:40:56 +0300

    Painter. Макет "Сцена"-"Табы для данных" Первичное размещение управления (поправил)

--- painter/gui.py
+++ painter/gui.py
@@ -13,7 +13,6 @@ from pathlib import Path
 from PySide2 import QtWidgets, QtGui
 
 
-
 class Painter(QtWidgets.QMainWindow):
 
     def __init__(self, parent=None) -> None:
@@ -30,6 +29,7 @@ class Painter(QtWidgets.QMainWindow):
         scale_ico = QtGui.QIcon(str(Path(os.getcwd()).parents[0]) + '/ico/scale.png')
         dist_ico = QtGui.QIcon(str(Path(os.getcwd()).parents[0]) + '/ico/polyline.png')
         area_ico = QtGui.QIcon(str(Path(os.getcwd()).parents[0]) + '/ico/area.png')
+        object_ico = QtGui.QIcon(str(Path(os.getcwd()).parents[0]) + '/ico/object.png')
         settings_ico = QtGui.QIcon(str(Path(os.getcwd()).parents[0]) + '/ico/settings.png')
         draw_ico = QtGui.QIcon(str(Path(os.getcwd()).parents[0]) + '/ico/draw.png')
         exit_ico = QtGui.QIcon(str(Path(os.getcwd()).parents[0]) + '/ico/exit.png')
@@ -39,129 +39,116 @@ class Painter(QtWidgets.QMainWindow):
         self.setWindowTitle('Painter')
         self.setWindowIcon(self.main_ico)
         # Центральный виджет
-            # создаем сетку из двух колонок
-            # окно для графики
-            # окно для ввода данных
+        # создаем сетку из двух колонок
+        # окно для графики
+        # окно для ввода данных
         central_widget = QtWidgets.QWidget()
         grid = QtWidgets.QGridLayout(self)
         grid.setColumnStretch(0, 7)
         grid.setColumnStretch(1, 1)
-            # В первой колонке создаем место под ген.план
-            # создаем сцену  #создаем сцену и плосы прокрутки картинки
+        # В первой колонке создаем место под ген.план
+        # создаем сцену  #создаем сцену и плосы прокрутки картинки
         self.scene = QtWidgets.QGraphicsScene(self)
-            # создаем полосы прокрутки
+        # создаем полосы прокрутки
         self.area = QtWidgets.QScrollArea(self)
-            # добавляем картинку
+        # добавляем картинку
         self.pixmap = QtGui.QPixmap()
         self.scene.addPixmap(self.pixmap)
-            # создаем обработчик клика мыши по сцене
+        # создаем обработчик клика мыши по сцене
         # self.scene.mousePressEvent = self.m_press_event
-            # создаем вид который визуализирует сцену
+        # создаем вид который визуализирует сцену
         self.view = QtWidgets.QGraphicsView(self.scene, self)
         self.area.setWidget(self.view)
         self.area.setWidgetResizable(True)
 
-            # т.к. данных  много создадим
-            # вкладки табов
+        # т.к. данных  много создадим
+        # вкладки табов
         self.tabs = QtWidgets.QTabWidget()  # создаем вкладки табов
         self.tab_main = QtWidgets.QWidget()  # 0. Главная вкладка с данными
         self.tab_settings = QtWidgets.QWidget()  # 1. Настройки
-            # добавляем "0" таб на вкладку табов
+        # добавляем "0" таб на вкладку табов
         self.tabs.addTab(self.tab_main, "")  # 0. Главная вкладка с данными
         self.tabs.setTabIcon(0, draw_ico)
         self.tabs.setTabToolTip(0, "Главня вкладка")
         self.tab_main.layout = QtWidgets.QFormLayout(self)
 
-            # Рамка №1 (то что будет в рамке 1)
+        # Рамка №1 (то что будет в рамке 1)
         self.scale_name = QtWidgets.QLineEdit()
         self.scale_name.setPlaceholderText("Масштаб")
         self.scale_name.setToolTip("[м, пикс.]")
         self.scale_name.setReadOnly(True)
 
-            # Рамка №2 (то что будет в рамке 2)
-        self.draw_act = QtWidgets.QRadioButton("Объект")
-        self.draw_act.setChecked(True)
-        self.scale_act = QtWidgets.QRadioButton("Масштаб")
-        self.dist_act = QtWidgets.QRadioButton("Расстояние")
-        self.area_act = QtWidgets.QRadioButton("Площадь")
+        # Рамка №2 (то что будет в рамке 2)
+        self.type_act = QtWidgets.QComboBox()  # д. проектируемый/существующий объект
+        self.type_act.addItems(["Объект", "Масштаб", "Расстояние", "Площадь"])
+        self.type_act.setItemIcon(0, object_ico)
+        self.type_act.setItemIcon(1, scale_ico)
+        self.type_act.setItemIcon(2, dist_ico)
+        self.type_act.setItemIcon(3, area_ico)
         self.result_lbl = QtWidgets.QLabel()
         self.draw_btn = QtWidgets.QPushButton("Применить")
         self.draw_btn.setCheckable(True)
         self.draw_btn.setChecked(False)
 
-            # Рамка №3 (то что будет в рамке 3)
-        self.scale_name = QtWidgets.QLineEdit()
-        self.scale_name.setPlaceholderText("Масштаб")
-        self.scale_name.setToolTip("[м, пикс.]")
-        self.scale_name.setReadOnly(True)
+        # Упаковываем все на вкладку таба "0" (делаем все в QGroupBox
+        # т.к. элементы будут добавляться и их
+        # потом нужно будет объединять в группы
 
-
-            # Упаковываем все на вкладку таба "0" (делаем все в QGroupBox
-            # т.к. элементы будут добавляться и их
-            # потом нужно будет объединять в группы
-
-            # Рамка №1
+        # Рамка №1
         layout_scale = QtWidgets.QFormLayout(self)
         GB_scale = QtWidgets.QGroupBox('Масштаб')
         GB_scale.setStyleSheet("QGroupBox { font-weight : bold; }")
         layout_scale.addRow("", self.scale_name)
         GB_scale.setLayout(layout_scale)
-            # Рамка №2
+        # Рамка №2
         layout_act = QtWidgets.QFormLayout(self)
         GB_act = QtWidgets.QGroupBox('Действие')
         GB_act.setStyleSheet("QGroupBox { font-weight : bold; }")
-        hbox_1 = QtWidgets.QHBoxLayout()
-        hbox_1.addWidget(self.draw_act)
-        hbox_1.addWidget(self.scale_act)
-        layout_act.addRow("", hbox_1)
-        hbox_2 = QtWidgets.QHBoxLayout()
-        hbox_2.addWidget(self.dist_act)
-        hbox_2.addWidget(self.area_act)
-        layout_act.addRow("", hbox_2)
-        layout_act.addRow("", self.result_lbl)
+        layout_act.addRow("", self.type_act)
         layout_act.addRow("", self.draw_btn)
+        layout_act.addRow("", self.result_lbl)
         GB_act.setLayout(layout_act)
-            # Собираем рамки
+        # Собираем рамки
         self.tab_main.layout.addWidget(GB_scale)
         self.tab_main.layout.addWidget(GB_act)
-            # Размещаем на табе
+        # Размещаем на табе
         self.tab_main.setLayout(self.tab_main.layout)
 
-            # добавляем "1" таб на вкладку табов
+        # добавляем "1" таб на вкладку табов
         self.tabs.addTab(self.tab_settings, "")  # 1. Настройки
         self.tabs.setTabIcon(1, settings_ico)
         self.tabs.setTabToolTip(1, "Настройки")
         self.tab_settings.layout = QtWidgets.QFormLayout(self)
-            # Рамка №1 (то что будет в рамке 1)
+        # Рамка №1 (то что будет в рамке 1)
         self.db_name = QtWidgets.QLineEdit()  # Наименование  базы данных
         self.db_name.setPlaceholderText("Наименование базы данных")
         self.db_name.setToolTip("Наименование базы данных")
         self.db_name.setReadOnly(True)
-            # Рамка №2 (то что будет в рамке 2)
+        # Рамка №2 (то что будет в рамке 2)
         self.plan_list = QtWidgets.QComboBox()  # ген.планы объекта
         self.plan_list.addItems(["--Нет ген.планов-- "])
         self.plan_list.setToolTip("""Ген.планы объекта""")
         # self.plan_list.activated[str].connect(self.plan_list_select)
 
-            # Упаковываем все на вкладку таба "0" (делаем все в QGroupBox
-            # т.к. элементы будут добавляться и их
-            # потом нужно будет объединять в группы
-            # Рамка №1
+        # Упаковываем все на вкладку таба "0" (делаем все в QGroupBox
+        # т.к. элементы будут добавляться и их
+        # потом нужно будет объединять в группы
+        # Рамка №1
         layout_db = QtWidgets.QFormLayout(self)
         GB_db = QtWidgets.QGroupBox('База данных')
         GB_db.setStyleSheet("QGroupBox { font-weight : bold; }")
         layout_db.addRow("", self.db_name)
         GB_db.setLayout(layout_db)
-            # Рамка №2
+        # Рамка №2
         layout_plan = QtWidgets.QFormLayout(self)
         GB_plan = QtWidgets.QGroupBox('Ген.план')
         GB_plan.setStyleSheet("QGroupBox { font-weight : bold; }")
         layout_plan.addRow("", self.plan_list)
         GB_plan.setLayout(layout_plan)
-            # Размещаем на табе
+        # Размещаем на табе
         self.tab_settings.layout.addWidget(GB_db)
         self.tab_settings.layout.addWidget(GB_plan)
-            # Размещаем на табе
+        # Размещаем на табе
         self.tab_settings.setLayout(self.tab_settings.layout)
 
         grid.addWidget(self.area, 0, 0, 1, 1)
@@ -175,7 +162,7 @@ class Painter(QtWidgets.QMainWindow):
         db_create.setStatusTip('Создать новую базу данных')
         db_create.triggered.connect(self.db_create)
         db_menu.addAction(db_create)
-        db_connect = QtWidgets.QAction(db_ico,'Подключиться', self)
+        db_connect = QtWidgets.QAction(db_ico, 'Подключиться', self)
         db_connect.setStatusTip('Подключиться к существующей базе данных')
         db_connect.triggered.connect(self.db_connect)
         db_menu.addAction(db_connect)
@@ -208,20 +195,6 @@ class Painter(QtWidgets.QMainWindow):
         plan_del.triggered.connect(self.plan_del)
         plan_menu.addAction(plan_del)
 
-        # Интрументы
-        tools_scale = QtWidgets.QAction(scale_ico, 'Масштаб', self)
-        tools_scale.setShortcut('Ctrl+M')
-        tools_scale.setStatusTip('Вычислить масштаб плана')
-        tools_scale.triggered.connect(self.tools_scale)
-        tools_dist = QtWidgets.QAction(dist_ico, 'Расстояние', self)
-        tools_dist.setShortcut('Ctrl+D')
-        tools_dist.setStatusTip('Вычислить произвольное расстояние')
-        tools_dist.triggered.connect(self.tools_dist)
-        tools_area = QtWidgets.QAction(area_ico, 'Площадь', self)
-        tools_area.setShortcut('Ctrl+F')
-        tools_area.setStatusTip('Вычислить произвольную площадь')
-        tools_area.triggered.connect(self.tools_area)
-
         # Выход из приложения
         exit_prog = QtWidgets.QAction(exit_ico, 'Выход', self)
         exit_prog.setShortcut('Ctrl+Q')
@@ -240,18 +213,12 @@ class Painter(QtWidgets.QMainWindow):
         about_prog.setStatusTip('О приложении Painter')
         about_prog.triggered.connect(self.about_programm)
 
-
         # Меню приложения (верхняя плашка)
         menubar = self.menuBar()
         file_menu = menubar.addMenu('Файл')
         file_menu.addMenu(db_menu)
         file_menu.addMenu(plan_menu)
         file_menu.addAction(exit_prog)
-        tools_menu = menubar.addMenu('Иструменты')
-        tools_menu.addAction(tools_scale)
-        tools_menu.addAction(tools_dist)
-        tools_menu.addAction(tools_area)
-
         help_menu = menubar.addMenu('Справка')
         help_menu.addAction(help_show)
         help_menu.addAction(about_prog)
@@ -288,7 +255,6 @@ class Painter(QtWidgets.QMainWindow):
     def plan_del(self):
         print("plan_del")
 
-
     #     Функция выхода из программы
     def close_event(self) -> None:
         messageBox = QtWidgets.QMessageBox(
@@ -307,18 +273,6 @@ class Painter(QtWidgets.QMainWindow):
         elif resultCode == QtWidgets.QMessageBox.Yes:
             return self.close()
 
-    # 2. Вкладка ИНСТРУМЕНТЫ
-    # Функция масштаба
-    def tools_scale(self):
-        print("tools_scale")
-
-    # Функция расстояния
-    def tools_dist(self):
-        print("tools_dist")
-
-    # Функция площади
-    def tools_area(self):
-        print("tools_area")
 
     # 2. Вкладка СПРАВКА
     # функция справки
@@ -337,7 +291,6 @@ class Painter(QtWidgets.QMainWindow):
         )
         messageBox.setWindowIcon(self.main_ico)
         messageBox.exec_()
-
 
 
 if __name__ == '__main__':
