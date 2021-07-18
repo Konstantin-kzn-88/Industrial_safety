aee43851622e6d7313b472a8222af1a170a348f2
konstantin <kuznetsovkm@yandex.ru>
2021-07-18 21:57:08 +0300

    Excel работа с активной книгой. Добавил работу с выделенным диапазоном)

--- sandbox/get_val_Excel.py
+++ sandbox/get_val_Excel.py
@@ -2,10 +2,10 @@ import win32com.client
 Excel = win32com.client.Dispatch("Excel.Application")
 print(Excel.ActiveWorkbook.Name)
 print(Excel.ActiveSheet.Name)
-print(Excel.ActiveSheet.Select)
 sheet = Excel.ActiveSheet
 
-vals = [r[0].value for r in sheet.Range("A1:A2")]
+# Excel.Selection.Value = "Hello World"
+vals = Excel.Selection.Value
 print(vals)
 
 if __name__ == '__main__':
