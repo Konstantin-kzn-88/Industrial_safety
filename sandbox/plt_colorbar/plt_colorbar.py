dfe9eac8725f4754cda246c9f6d3401c9f87b669
Konstantin <kuznetsovkm@yandex.ru>
2021-08-06 17:46:17 +0300

    Sandbox. plt_color доделал)

--- sandbox/plt_colorbar/plt_colorbar.py
+++ sandbox/plt_colorbar/plt_colorbar.py
@@ -1,16 +1,35 @@
-
 import matplotlib.pyplot as plt
 
-fig, ax = plt.subplots()
 
-sc = ax.scatter([1, 2], [1, 2], c=[1, 3], cmap='jet')
-ax.set_ylabel('YLabel', loc='top')
-ax.set_xlabel('XLabel', loc='left')
+image = plt.imread('test200.png')
+plt.imshow(image)
+vals = []
+for i in range(0, 201,1):
+    if i < 50:
+        val_list = [0.0000033] * 200
+    elif i < 100:
+        val_list = [0.0000077] * 200
+    elif i < 150:
+        val_list = [0.0000099] * 200
+    vals.append(val_list)
 
-cbar = fig.colorbar(sc)
-cbar.set_label("ZLabel", loc='top')
 
+plt.pcolormesh (vals, cmap=plt.get_cmap('jet'), alpha=0.4)# levels=levels сглаживание
+plt.axis('off')
+plt.colorbar()
 plt.show()
+# import matplotlib.pyplot as plt
+#
+# fig, ax = plt.subplots()
+#
+# sc = ax.scatter([1, 2], [1, 2], c=[1, 3], cmap='hsv')
+# ax.set_ylabel('YLabel', loc='top')
+# ax.set_xlabel('XLabel', loc='left')
+#
+# cbar = fig.colorbar(sc)
+# cbar.set_label("ZLabel", loc='top')
+#
+# plt.show()
 
 # import matplotlib.pyplot as plt
 # im = plt.imread('test.png')
