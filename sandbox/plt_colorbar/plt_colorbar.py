e0163bfbc6e99592ea6415734b1751a2ef585a43
konstantin <kuznetsovkm@yandex.ru>
2021-08-08 22:21:39 +0300

    Sandbox. none

--- sandbox/plt_colorbar/plt_colorbar.py
+++ sandbox/plt_colorbar/plt_colorbar.py
@@ -1,23 +1,44 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import colors
+from PIL import Image
 
 
-image = plt.imread('test200.png')
-plt.imshow(image)
-vals = []
-for i in range(0, 201,1):
-    if i < 50:
-        val_list = [0.0000033] * 200
-    elif i < 100:
-        val_list = [0.0000077] * 200
-    elif i < 150:
-        val_list = [0.0000099] * 200
-    vals.append(val_list)
-
-
-plt.pcolormesh (vals, cmap=plt.get_cmap('jet'), alpha=0.4)# levels=levels сглаживание
-plt.axis('off')
+# 1. Построить изображение с данными с нулевым альфа каналом
+im = plt.imread('test.png')
+implot = plt.imshow(im)
+vals = np.random.randint(3, size=(200, 200))
+plt.pcolormesh(vals, cmap=plt.get_cmap('jet'), alpha=0)  # levels=levels сглаживание
+# plt.axis('off')
 plt.colorbar()
-plt.show()
+plt.savefig('saved_figure.png')
+plt.clf()
+# 2. Построить по данными цветовую карту с альфа равным 1
+vals = np.random.randint(3, size=(200, 200))
+plt.pcolormesh(vals, cmap=plt.get_cmap('jet'), alpha=1)  # levels=levels сглаживание
+# plt.axis('off')
+plt.colorbar()
+plt.savefig('saved_figure.png')
+img = Image.open('saved_figure.png')
+pixdata = img.load()
+width, height = img.size
+for y in range(height):
+    for x in range(width):
+        print(pixdata[x,y])
+img.save("saved_figure.png", "PNG")
+# 3.Наложить два изображения
+# filename = 'saved_figure2.png'
+# ironman = Image.open(filename, 'r')
+# filename1 = 'saved_figure.png'
+# bg = Image.open(filename1, 'r')
+# text_img = Image.new('RGBA', (600,500), (0, 0, 0, 0))
+# text_img.paste(bg, (0,0))
+# text_img.paste(ironman, (0,0), mask=ironman)
+# text_img.save("ball.png", format="png")
+
+
+
+
 # import matplotlib.pyplot as plt
 #
 # fig, ax = plt.subplots()
