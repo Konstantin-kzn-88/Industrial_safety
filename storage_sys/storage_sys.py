d77af5ef7984ae85a316f07c2b619b4f2666dbe1
konstantin <kuznetsovkm@yandex.ru>
2021-08-21 10:08:23 +0300

    Storage_sys. FOREIGN KEY

--- storage_sys/storage_sys.py
+++ storage_sys/storage_sys.py
@@ -4,7 +4,119 @@ def create_all_tabble():
     create_company_table()
     create_opo_table()
     create_documentation_table()
+    create_line_obj_table()
+    create_state_obj_table()
+    create_build_obj_table()
+    create_project_table()
 
+
+def create_project_table():
+    """
+    Создание седьмой таблицы "Проект".
+    Удаление производится для того что-бы не опасть на ошибку
+    """
+    with sq.connect("data.db") as con:
+        cur = con.cursor()
+
+        # DEL
+        cur.execute("DROP TABLE IF EXISTS project")  # will delete the table if it exists
+
+        # CREATE
+        cur.execute("""CREATE TABLE IF NOT  EXISTS project (
+            project_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            opo_id INTEGER NOT NULL,
+            line_obj_id INTEGER,
+            state_obj_id INTEGER,
+            build_obj_id INTEGER,
+            name_project TEXT NOT NULL,
+            date_project DATE NOT NULL, 
+            pz_project BLOB NOT NULL,
+            pzu_project BLOB NOT NULL,
+            kr_project BLOB NOT NULL,
+            ios_project BLOB NOT NULL,
+            pos_project BLOB NOT NULL,
+            another_project BLOB NOT NULL  
+            )""")
+
+def create_build_obj_table():
+    """
+    Создание шестой таблицы "Здания/сооружения".
+    Удаление производится для того что-бы не опасть на ошибку
+    """
+    with sq.connect("data.db") as con:
+        cur = con.cursor()
+
+        # DEL
+        cur.execute("DROP TABLE IF EXISTS build_obj")  # will delete the table if it exists
+
+        # # CREATE
+        # cur.execute("""CREATE TABLE IF NOT  EXISTS build_obj (
+        #     build_obj_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        #     name_obj TEXT NOT NULL,
+        #     status TEXT NOT NULL,
+        #     date_manufacture DATE NOT NULL,
+        #     date_entry DATE NOT NULL,
+        #     date_upto DATE NOT NULL,
+        #     opo_id INTEGER NOT NULL,
+        #     FOREIGN KEY (opo) REFERENCES opo(opo_id));
+        #     )""")
+
+def create_state_obj_table():
+    """
+    Создание пятой таблицы "Стационарные объекты".
+    Удаление производится для того что-бы не опасть на ошибку
+    """
+    with sq.connect("data.db") as con:
+        cur = con.cursor()
+
+        # DEL
+        cur.execute("DROP TABLE IF EXISTS state_obj")  # will delete the table if it exists
+
+        # CREATE
+        cur.execute("""CREATE TABLE IF NOT  EXISTS state_obj (
+            state_obj_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            opo_id INTEGER NOT NULL,
+            reg_number INTEGER NOT NULL,
+            name_obj TEXT NOT NULL,
+            substance TEXT NOT NULL,
+            volume INTEGER NOT NULL,
+            temperature INTEGER NOT NULL,
+            pressure INTEGER NOT NULL,
+            alpha INTEGER NOT NULL,
+            status TEXT NOT NULL,
+            date_manufacture DATE NOT NULL,
+            date_entry DATE NOT NULL,
+            date_upto DATE NOT NULL,   
+            passport BLOB NOT NULL  
+            )""")
+
+def create_line_obj_table():
+    """
+    Создание четвертой таблицы "Линейные объекты".
+    Удаление производится для того что-бы не опасть на ошибку
+    """
+    with sq.connect("data.db") as con:
+        cur = con.cursor()
+
+        # DEL
+        cur.execute("DROP TABLE IF EXISTS line_obj")  # will delete the table if it exists
+
+        # CREATE
+        cur.execute("""CREATE TABLE IF NOT  EXISTS line_obj (
+            line_obj_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            opo_id INTEGER NOT NULL,
+            reg_number INTEGER NOT NULL,
+            name_obj TEXT NOT NULL,
+            substance TEXT NOT NULL,
+            lenght INTEGER NOT NULL,
+            diameter INTEGER NOT NULL,
+            pressure INTEGER NOT NULL,
+            status TEXT NOT NULL,
+            date_manufacture DATE NOT NULL,
+            date_entry DATE NOT NULL,
+            date_upto DATE NOT NULL,   
+            passport BLOB NOT NULL  
+            )""")
 
 def create_documentation_table():
     """
@@ -18,14 +130,17 @@ def create_documentation_table():
         cur.execute("DROP TABLE IF EXISTS documentation")  # will delete the table if it exists
 
         # CREATE
-        cur.execute("""CREATE TABLE IF NOT  EXISTS documentation (
-            doc_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            opo_id INTEGER NOT NULL,
-            type_doc TEXT NOT NULL,
-            reg_doc TEXT NOT NULL,
-            date_doc YEAR NOT NULL   
-            doc BLOB NOT NULL  
-            )""")
+        cur.execute("""CREATE TABLE IF NOT EXISTS opo (
+                            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                            type_doc TEXT NOT NULL,
+                            reg_doc TEXT NOT NULL,
+                            date_doc DATE NOT NULL, 
+                            doc BLOB NOT NULL,
+                            id_opo INTEGER NOT NULL, 
+                            FOREIGN KEY (id_opo) REFERENCES opo(id));
+                    """)
+
+
 
 def create_opo_table():
     """
@@ -39,14 +154,16 @@ def create_opo_table():
         cur.execute("DROP TABLE IF EXISTS opo")  # will delete the table if it exists
 
         # CREATE
-        cur.execute("""CREATE TABLE IF NOT  EXISTS opo (
-            opo_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            company_id INTEGER NOT NULL,
-            name_opo TEXT NOT NULL,
-            address_opo TEXT NOT NULL,
-            reg_number_opo TEXT NOT NULL,
-            class_opo TEXT NOT NULL
-            )""")
+        cur.execute("""CREATE TABLE IF NOT EXISTS opo (
+                            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                            name_opo TEXT NOT NULL,
+                            address_opo TEXT NOT NULL,
+                            reg_number_opo TEXT NOT NULL,
+                            class_opo TEXT NOT NULL,
+                            id_company INTEGER NOT NULL, 
+                            FOREIGN KEY (id_company) REFERENCES company(id));
+                    """)
+
 
 def create_company_table():
     """
@@ -61,7 +178,7 @@ def create_company_table():
 
         # CREATE
         cur.execute("""CREATE TABLE IF NOT  EXISTS company (
-            company_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
             name_company TEXT NOT NULL,
             full_name_manager TEXT NOT NULL,
             ur_address TEXT NOT NULL,
