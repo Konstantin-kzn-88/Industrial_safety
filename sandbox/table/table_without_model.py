71848c0f9be3cd993b12781c6dc5288e784df8a4
Konstantin <kuznetsovkm@yandex.ru>
2022-03-29 18:05:18 +0300

    QTableWidget работа с таблицей. Без модели. Добавил чтение и заполнение таблицы

--- sandbox/table/table_without_model.py
+++ sandbox/table/table_without_model.py
@@ -13,7 +13,7 @@ class Table(QWidget):
         self.resize(400, 300)
         layout = QVBoxLayout()
 
-        self.TableWidget = QTableWidget(1, 3)
+        self.TableWidget = QTableWidget(0, 3)
 
         # Заголовки.
         self.TableWidget.setHorizontalHeaderLabels(['Поз.', 'Объем, м3', 'Расположение'])
@@ -24,27 +24,17 @@ class Table(QWidget):
         del_button.clicked.connect(self.del_button_func)
         save_button = QPushButton("Save")
         save_button.clicked.connect(self.save_button_func)
+        append_button = QPushButton("Append")
+        append_button.clicked.connect(self.append_button_func)
 
         # self.TableWidget.itemSelectionChanged.connect(self.save_button_func)
         self.TableWidget.clicked[QModelIndex].connect(self.clicked)
-
-        # Добавить данные
-        newItem = QTableWidgetItem('Чжан Сан')
-
-        self.TableWidget.setItem(0, 0, newItem)
-
-        newItem = QTableWidgetItem('Male')
-
-
-        self.TableWidget.setItem(0, 1, newItem)
-
-        newItem = QTableWidgetItem('160')
-        self.TableWidget.setItem(0, 2, newItem)
 
         layout.addWidget(self.TableWidget)
         layout.addWidget(add_button)
         layout.addWidget(del_button)
         layout.addWidget(save_button)
+        layout.addWidget(append_button)
 
         self.setLayout(layout)
 
@@ -59,13 +49,45 @@ class Table(QWidget):
         self.TableWidget.insertRow(value)
 
     def save_button_func(self):
-        print("save")
+        data_list = []
+        # Записать данные
+        count_row = 0 # начинаем с 0 строки
+
+        for _ in range(0, self.TableWidget.rowCount()):  # посчитаем строки
+            append_list = [] # заведем пустой список для объекта
+            count_col = 0  # колонка с индесом 0
+            for _ in range(0, self.TableWidget.columnCount()):  # для каждого столбца строки
+                append_list.append(self.TableWidget.item(count_row, count_col).text()) # добавим в словарь текст ячейки
+                count_col += 1  # + 1 к столбцу
+            data_list.append(append_list) # добавим объект
+            count_row += 1  # +1 к строке (новая строка если len(data_list) > 1)
+        print(data_list)
+
+
 
     def clicked(self, index):
         print(index.row())
         # item = self.TableWidget.itemFromIndex(index)
         # print(item)
         # print(item.column(), item.row())  # получить столбец/строку   по индексу
+
+    def append_button_func(self):
+
+        data_list = [['T-3', '100', 'Under'], ['T-4', '400', 'On']]
+        # Добавить данные
+        count_row = self.TableWidget.rowCount()  # посчитаем количество строк
+
+        for item in data_list:  # возьмем каждый словарь из data_list
+            count_col = 0  # колонка с индесом 0
+            # вставим строку, т.е. если строк 0, то на 0 позицию по индексу,
+            # если кол-во строк 1 то на 1 позицию по индексу
+            self.TableWidget.insertRow(count_row)
+            for var in item:  # для каждого значения из словаря item пробежим по столбцам
+                TableWidgetItem = QTableWidgetItem(var)
+                self.TableWidget.setItem(count_row, count_col, TableWidgetItem)
+                count_col += 1  # + 1 к столбцу
+            count_row += 1  # +1 к строке (новая строка если len(data_list) > 1)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
