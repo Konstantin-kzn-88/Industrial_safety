ea3e51f9c4ff9d96812b59c1a495c939ea945138
Konstantin <kuznetsovkm@yandex.ru>
2022-05-26 17:12:11 +0300

    TOXI получил рисунок на основе данных экселя

--- toxi/toxic_dispersion.py
+++ toxi/toxic_dispersion.py
@@ -1,7 +1,6 @@
 import sys
-from PySide2.QtWidgets import (QWidget, QHBoxLayout,
-                               QLabel, QApplication, QMessageBox)
-from PySide2.QtGui import QPixmap, QImage, QPainter, QGuiApplication, QColor
+
+from PySide2 import QtGui
 import traceback
 from pathlib import Path
 import xlwings as xw
@@ -31,6 +30,76 @@ def get_data_in_excel(row: tuple, col: tuple) -> list:
         ndim=2).value  # с 2 - т.к. нужно ислючить размерность длины по X, Y
 
 
+def set_pix_color(qimg_zone, max_el, value, x ,y):
+    if value >= max_el:
+        qimg_zone.setPixelColor(x, y, QtGui.QColor(255, 0, 0, 255))
+    #     красный
+    elif max_el * 1.00 > value >= max_el * 0.003:
+        qimg_zone.setPixelColor(x, y, QtGui.QColor(255, 10, 0, 255))
+    # рыжий
+    elif max_el * 0.003 > value >= max_el * 0.001:
+        qimg_zone.setPixelColor(x, y, QtGui.QColor(255, 40, 0, 255))
+
+    # желтый
+    elif max_el * 0.001 > value >= max_el * 0.00005:
+        qimg_zone.setPixelColor(x, y, QtGui.QColor(255, 170, 0, 255))
+
+    # салатовый
+    elif max_el * 0.00005 > value >= max_el * 0.000005:
+        qimg_zone.setPixelColor(x, y, QtGui.QColor(230, 255, 0, 255))
+
+    # зеленый
+    elif max_el * 0.000005 > value >= max_el * 0.0000005:
+        qimg_zone.setPixelColor(x, y, QtGui.QColor(100, 255, 0, 255))
+
+    # голубой
+    elif max_el * 0.0000005 > value >= max_el * 0.00000005:
+        qimg_zone.setPixelColor(x, y, QtGui.QColor(0, 255, 40, 255))
+
+
+    # # темно-голубой
+    # elif max_el * 0.01 > value >= max_el * 0.009:
+    #     qimg_zone.setPixelColor(x, y, QtGui.QColor(0, 220, 255, 255))
+    # elif max_el * 0.009 > value >= max_el * 0.008:
+    #     qimg_zone.setPixelColor(x, y, QtGui.QColor(0, 210, 255, 255))
+    # elif max_el * 0.008 > value >= max_el * 0.007:
+    #     qimg_zone.setPixelColor(x, y, QtGui.QColor(0, 200, 255, 255))
+    # elif max_el * 0.007 > value >= max_el * 0.006:
+    #     qimg_zone.setPixelColor(x, y, QtGui.QColor(0, 190, 255, 255))
+    # elif max_el * 0.006 > value >= max_el * 0.005:
+    #     qimg_zone.setPixelColor(x, y, QtGui.QColor(0, 160, 255, 255))
+    # elif max_el * 0.005 > value >= max_el * 0.004:
+    #     qimg_zone.setPixelColor(x, y, QtGui.QColor(0, 150, 255, 255))
+    # #     синий
+    # elif max_el * 0.004 > value >= max_el * 0.003:
+    #     qimg_zone.setPixelColor(x, y, QtGui.QColor(0, 130, 255, 255))
+    # elif max_el * 0.003 > value >= max_el * 0.0025:
+    #     qimg_zone.setPixelColor(x, y, QtGui.QColor(0, 110, 255, 255))
+    # elif max_el * 0.0025 > value >= max_el * 0.0015:
+    #     qimg_zone.setPixelColor(x, y, QtGui.QColor(0, 90, 255, 255))
+    # elif max_el * 0.0015 > value >= max_el * 0.001:
+    #     qimg_zone.setPixelColor(x, y, QtGui.QColor(0, 70, 255, 255))
+    # elif max_el * 0.001 > value >= max_el * 0.0008:
+    #     qimg_zone.setPixelColor(x, y, QtGui.QColor(0, 50, 255, 255))
+    # elif max_el * 0.0008 > value >= max_el * 0.0007:
+    #     qimg_zone.setPixelColor(x, y, QtGui.QColor(0, 40, 255, 255))
+    # elif max_el * 0.0007 > value >= max_el * 0.0006:
+    #     qimg_zone.setPixelColor(x, y, QtGui.QColor(0, 30, 255, 255))
+    # elif max_el * 0.0006 > value >= max_el * 0.0002:
+    #     qimg_zone.setPixelColor(x, y, QtGui.QColor(0, 20, 255, 255))
+    # elif max_el * 0.0002 > value >= max_el * 0.0001:
+    #     qimg_zone.setPixelColor(x, y, QtGui.QColor(0, 0, 255, 255))
+
+def get_max_elem(data:list):
+    max_elem = 0
+    for list_ in data:
+        i = max(list_)
+        if i>max_elem:
+            max_elem = i
+    print(max_elem)
+    return max_elem
+
+
 DATA_PATH = Path.cwd()
 SCALE = 1.443  # т.е. в 1 метре 1.443 пикселя
 
@@ -44,21 +113,18 @@ if row != 0 and col != 0:
 else:
     val = 0
 
-print(row, col)
 
-app = QGuiApplication([])
-pixmap = QPixmap(row[0], col[1])
-pixmap.fill(QColor(255, 255, 255, 255))
+
+app = QtGui.QGuiApplication([])
+pixmap = QtGui.QPixmap(row[0], col[1])
+pixmap.fill(QtGui.QColor(255, 255, 255, 255))
 image_zone = pixmap.toImage()  #
 
-print(row[0], col[1])
+max_elem = get_max_elem(val)
 
-for i in range(0, col[1]-1):
-
-    for j in val[i]:
-
-        if val[i][val[i].index(j)] > 0:
-            image_zone.setPixelColor(j, i, QColor(0, 0, 255, 255))
+for row in range(0, col[1]-1):
+    for iter in range(0, len(val[row])-1):
+        set_pix_color(image_zone, max_elem, val[row][iter], iter, row)
 
 image_zone.save('cat.jpg')
 
