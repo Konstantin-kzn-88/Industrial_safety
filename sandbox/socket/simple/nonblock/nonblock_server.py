1d6fead4f402c7d4e77a62d3d33e749674a50715
Konstantin <kuznetsovkm@yandex.ru>
2022-06-01 17:44:14 +0300

    socketserver принятие и отправка

---
+++ sandbox/socket/simple/nonblock/nonblock_server.py
@@ -0,0 +1,28 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(("127.0.0.1", 8888))
+sock.listen(5)
+sock.setblocking(False)
+
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(("127.0.0.1", 8888))
+sock.listen(5)
+sock.setblocking(False)
+
+# лучше делать через менеджер контекста with
+while True:
+    try:
+        client, addr = sock.accept()
+    except socket.error:
+        print("No client")
+    except KeyboardInterrupt:
+        break
+    else:
+        client.setblocking(True)
+        result = client.recv(1024)
+        client.close()
+        print('Messege', result.decode('utf-8'))
+
